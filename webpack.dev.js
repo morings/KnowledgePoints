f98264dd63c1bc0f6c628785903ac97e124aa292
star <1620201780@qq.com>
2020-11-14 21:33:30 +0800

    增加devtool

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const config = require("./webpack.config.js")
 const {merge} = require("webpack-merge")
 module.exports = merge(config,{
   mode:"development",
+  devtool:'source-map',
   devServer:{
     publicPath:'/dist/'
   }
